3f223165d71038834c55eb16414c9acca9ffd0c4
Dongook <dongjk9696@gmail.com>
2025-02-08 21:10:59 +0900

    Question two complete

--- Practical One.R
+++ Practical One.R
@@ -12,3 +12,22 @@ data = airquality
 # Row numbers with missing values
 data_missing <- data[!complete.cases(data), ]
 rownames(data_missing)
+
+# ============================
+# Question Two
+# ============================
+
+# Rows without any missing values
+data_clean <- data[complete.cases(data), ]
+
+# mean, sd, min, max of Ozone level
+mean(data_clean$Ozone)
+sd(data_clean$Ozone)
+min(data_clean$Ozone)
+max(data_clean$Ozone)
+
+# mean, sd, min, max of temperature
+mean(data_clean$Temp)
+sd(data_clean$Temp)
+min(data_clean$Temp)
+max(data_clean$Temp)
