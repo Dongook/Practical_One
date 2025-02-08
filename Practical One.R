92ed283875beeae596191f2f4c9e710762bf9b46
Dongook <dongjk9696@gmail.com>
2025-02-08 21:08:10 +0900

    Question one complete

--- Practical One.R
+++ Practical One.R
@@ -1,3 +1,14 @@
 # ============================
 # Practical One
 # ============================
+
+# ============================
+# Question One
+# ============================
+
+#Airquality dataset
+data = airquality
+
+# Row numbers with missing values
+data_missing <- data[!complete.cases(data), ]
+rownames(data_missing)
